e5a4aa4c82618e9349d5f0a7fac1b8e40c3ac8a0
D Lee <dlee1579@gmail.com>
2024-07-24 15:23:48 -0400

    added firebase SDK

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,29 @@ import { useState } from 'react'
 import './App.css'
 import auctionValues from "./assets/fantasy-auction-values-2024.json";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBP3VMZJa6D9zxDD2mmeMMoUfNlWt7qofw",
+  authDomain: "mocktion-draft.firebaseapp.com",
+  databaseURL: "https://mocktion-draft.firebaseio.com",
+  projectId: "mocktion-draft",
+  storageBucket: "mocktion-draft.appspot.com",
+  messagingSenderId: "605279740949",
+  appId: "1:605279740949:web:117804e59e5093ae0a1a1c",
+  measurementId: "G-QZF2XM205D"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   auctionValues.sort((a,b) => b.Price - a.Price)
   const [currentAvailablePlayers, setCurrentAvailablePlayers] = useState(auctionValues);
