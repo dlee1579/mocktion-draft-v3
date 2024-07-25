a7cbadc3fd4dbb8454ae06d4fe803b00208341af
D Lee <dlee1579@gmail.com>
2024-07-25 13:57:25 -0400

    select dropdown text size enlarged

--- src/pages/Draft.jsx
+++ src/pages/Draft.jsx
@@ -368,8 +368,8 @@ export default function Draft() {
                     <p>Remaining Budget: {getRemainingBudget()}</p>
                 </div>
                 <br></br>
-                <label htmlFor="filter-position" className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Filter by Position</label>
-                <select id="filter-position" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={filterPosition} onChange={handleChangeFilterPosition}>
+                <label htmlFor="filter-position" className='block mb-2 text-lg font-medium text-gray-900 dark:text-white'>Filter by Position</label>
+                <select id="filter-position" className="text-lg bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={filterPosition} onChange={handleChangeFilterPosition}>
                     <option value="ALL">ALL</option>
                     <option value="QB">QB</option>
                     <option value="RB">RB</option>
